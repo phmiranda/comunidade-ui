120448fe0826807f2772ed3c72c8533a639c2add
phmiranda <pehhagah.1607@gmail.com>
2022-04-07 15:53:25 -0300

    CONFIGURAÇÃO
    Criação das camadas necessárias para o projeto e configurações dos arquivos utilizados para codificação localmente.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth/auth.component';
-import { CursoComponent } from './component/curso/curso.component';
-import { DuvidaComponent } from './component/duvida/duvida.component';
-import { RespostaComponent } from './component/resposta/resposta.component';
-import { UsuarioComponent } from './component/usuario/usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AuthComponent,
-    CursoComponent,
-    DuvidaComponent,
-    RespostaComponent,
-    UsuarioComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
